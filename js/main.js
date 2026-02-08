3c53c55729be9126aaec5997000427fd9524f3e6
glitchfer <arif.0496.rahman@gmail.com>
2026-02-08 20:16:48 +0700

    up

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+
+import { setupProdMode } from "./prod.js"; 
+
 window.__PROD__ =
   location.hostname.includes("netlify") ||
   location.hostname.includes("your-domain.com");
-
-import { setupProdMode } from "./prod.js";  
 
 setupProdMode();
